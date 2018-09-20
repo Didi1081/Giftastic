72ae84b6b1e411ce5595c60e66b21f811404d4c8
Rashmi <rashmi@Rashmis-MacBook-Pro.local>
2018-09-19 21:49:20 -0500

    Giftastic styling

---
+++ assets/javascript/gifs.js
@@ -0,0 +1,27 @@
+//6.1 - Always read documentation for that API
+
+$('button').on('click',function(){
+var x = $(this).data("animal");
+console.log(x);
+var queryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=T3bTJBKugMxVT3yX9ddzafzVAJTHEZtk&limit=1";
+
+$.ajax({url:queryURL,method:"GET"})
+       .done(function(response){
+           console.log(response);
+           for(var i=0;i<response.data.length;i++){
+               var animalDiv = $('<div>');
+               var animalImage = $('<img>');
+               animalImage.attr('src',response.data[i].images.fixed_height.url);
+               animalDiv.prepend(animalImage);
+               $('#gifsGoHere').prepend(animalDiv);
+
+               $('.border').gifplayer();
+
+
+
+
+
+           }
+       })
+
+})
